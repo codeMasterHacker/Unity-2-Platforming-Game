43196160dac88838c7beae5aba4c8e6da398f6c2
codeMasterHacker <46474900+codeMasterHacker@users.noreply.github.com>
2021-02-12 21:57:17 -0800

    added heart and green potion script

---
+++ Assets/Scripts/GreenPotion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenPotion : Collectable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        collectableName = "Green Potion";
+        description = "decreases health by 10";
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    override public void Use()
+    {
+        player.GetComponent<playerManager>().ChangeHealth(-10);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Heart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : Collectable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        collectableName = "Heart";
+        description = "increase health by 10";
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    override public void Use()
+    {
+        player.GetComponent<playerManager>().ChangeHealth(10);
+        Destroy(this.gameObject);
+    }
+}
